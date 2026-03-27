44c730dce9e8da66f65c37e805b3190407ae44aa
soha-sheikh <anjumsheikh072@gmail.com>
2026-03-27 18:01:55 +0530

    commit on master repo

--- calculate.php
+++ calculate.php
@@ -2,10 +2,10 @@
 
 $totalAmount = 1000;   
 $discountRate = 0.10;  
-$taxRate = 0.05; // 5% tax
+$taxRate = 0.05; 
 
 function getFinalPrice($total, $discountRate, $taxRate) {
-    // 1️⃣ Calculate discount
+   
     $discount = $total * $discountRate;        
     $subtotal = $total - $discount;  
     $tax = $subtotal * $taxRate;
